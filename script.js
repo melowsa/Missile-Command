70f331edb65b3dd418db2427581f189f8105d1ed
melowsa <70744420+melowsa@users.noreply.github.com>
2021-01-15 11:33:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let player = null;
 let playerSpeed = 5;
+let explosions = [];
+let shootTimer = 0;
+let explosionLife = 100;
+let shotsPerSecond = 2;
 
 function setup() {
     createCanvas(400, 400);
 
     player = createSprite(width / 2, height / 2, 20, 20);
     player.draw = DrawPlayer;
+
 }
 
 function draw() {
@@ -13,7 +18,39 @@ function draw() {
 
     MovePlayer();
 
+    Shoot();
+
+    RemoveDeadExplosions();
+
     drawSprites();
+}
+
+function RemoveDeadExplosions() {
+    if (explosions.length > 0 && explosions[0].life == 0) {
+        explosions.shift();
+    }
+        
+}
+
+function Shoot() {
+    shootTimer += deltaTime;
+    if (keyIsDown(32) && shootTimer >= 1000 / shotsPerSecond) {
+        CreateExplosion(player.position.x, player.position.y);
+        shootTimer = 0;
+    }
+}
+
+function CreateExplosion(x, y) {
+    let explosion = createSprite(x, y, 5, 5);
+    explosion.life = 100;
+    explosions.push(explosion);
+    explosion.draw = DrawExplosion;
+}
+
+function DrawExplosion() {
+    circle(0, 0, this.width);
+    this.width++;
+    this.height++;
 }
 
 function DrawPlayer() {
